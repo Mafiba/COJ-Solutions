a2b0f33bffa21f8dda3f2dad6ae10afbfdf8c09f
Mafiba <maufb04@hotmail.com>
2017-03-19 12:29:11 -0600

    Add files via upload

---
+++ 1000.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sum = Console.ReadLine().Split();
+            int a = int.Parse(sum[0]);
+            int b = int.Parse(sum[1]);
+            Console.WriteLine(a + b);
+        }
+    }
+}
---
+++ 1805.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = Console.ReadLine().Split();
+            ulong a = ulong.Parse(num[0]);
+            ulong b = ulong.Parse(num[1]);
+            Console.WriteLine((a + b) + (a - b) + (b + a) + (b - a));
+        }
+    }
+}
